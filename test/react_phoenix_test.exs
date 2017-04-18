d2f3a3f286133bcb3d46f03ce0e39cafc8185e0b
Geoffrey Lessel <geo@geoffreylessel.com>
2017-04-17 18:23:53 -0700

    Add ReactPhoenix tests

--- test/react_phoenix_test.exs
+++ test/react_phoenix_test.exs
@@ -1,8 +1,35 @@
 defmodule ReactPhoenixTest do
   use ExUnit.Case
   doctest ReactPhoenix
+  import ReactPhoenix
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "react_component returns a safe tuple" do
+    assert {:safe, _} = react_component("test")
+  end
+
+  test "react_component returns a renderable div" do
+    html = Phoenix.HTML.safe_to_string(react_component("test"))
+    assert String.match?(html, ~r|^<div.*></div>$|)
+  end
+
+  test "react_component returns a renderable div with data-react-class containing component name" do
+    html = Phoenix.HTML.safe_to_string(react_component("test"))
+    assert String.match?(html, ~r|data-react-class="test"|)
+  end
+
+  test "react_component returns a renderable div with data-react-props containing props list" do
+    html = Phoenix.HTML.safe_to_string(react_component("test", my: "props"))
+    expected = "<div data-react-class=\"test\" data-react-props=\"{&quot;my&quot;:&quot;props&quot;}\"></div>"
+    assert html == expected
+  end
+
+  test "react_component returns a renderable div with data-react-props containing props map" do
+    html = Phoenix.HTML.safe_to_string(react_component("test", %{my: "props"}))
+    expected = "<div data-react-class=\"test\" data-react-props=\"{&quot;my&quot;:&quot;props&quot;}\"></div>"
+    assert html == expected
   end
 end
