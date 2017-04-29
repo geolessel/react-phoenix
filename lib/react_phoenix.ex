8af2cc3b364386ae1e58e1e419b62d2c9b8fcffb
Geoffrey Lessel <geo@geoffreylessel.com>
2017-04-29 14:04:22 -0700

    Provide documentation on depricated ReactPhoenix functions

--- lib/react_phoenix.ex
+++ lib/react_phoenix.ex
@@ -1,6 +1,21 @@
 defmodule ReactPhoenix do
-  @moduledoc false
+  @moduledoc """
+  Functions for rendering of React.js components.
+  """
 
+  @doc """
+  (Depricated) provides functions for client-side rendering of React.js components
+
+  As of v0.4.0, please use `ElixirCabinet.ClientSide.react_component/2` instead. Provided for backward
+  compatability for versions < 0.4.0.
+  """
   def react_component(name, props \\ %{}), do: ReactPhoenix.ClientSide.react_component(name, props)
+
+  @doc """
+  (Depricated) provides functions for client-side rendering of React.js components
+
+  As of v0.4.0, please use `ElixirCabinet.ClientSide.react_component/3` instead. Provided for backward
+  compatability for versions < 0.4.0.
+  """
   def react_component(name, props, opts), do: ReactPhoenix.ClientSide.react_component(name, props, opts)
 end
