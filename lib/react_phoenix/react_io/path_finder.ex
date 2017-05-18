f74ff37185fe5614f20f70d566b92d95c98bded1
Geoffrey Lessel <geo@geoffreylessel.com>
2017-05-17 19:53:59 -0700

    fix pathfinder

--- lib/react_phoenix/react_io/path_finder.ex
+++ lib/react_phoenix/react_io/path_finder.ex
@@ -1,15 +1,16 @@
 defmodule ReactPhoenix.ReactIo.PathFinder do
   @moduledoc false
   @app_dir File.cwd!
-  @phoenix_1_3_react_stdio_path Path.join(~w(#{@app_dir} assets node_modules .bin react-stdio))
-  @phoenix_1_2_react_stdio_path Path.join(~w(#{@app_dir} node_modules .bin react-stdio))
+  @phoenix_1_3_react_stdio_path Path.join(~w(#{@app_dir} .. .. assets node_modules .bin react-stdio))
+  @phoenix_1_2_react_stdio_path Path.join(~w(#{@app_dir} .. .. node_modules .bin react-stdio))
   @local_react_stdio_path Path.join(~w(node_modules .bin react-stdio))
 
   def react_stdio_path(), do: react_stdio_path(default_locations())
   def react_stdio_path(locations) when is_list(locations), do: do_react_stdio_path(locations)
 
-  def do_react_stdio_path([]), do: raise("#{ReactPhoenix}: An installation of react-stdio cannot be found. Please run `npm install` and/or set the :react_stdio_path config value for :react_pheonix in your application.")
-  def do_react_stdio_path([location | rest]) do
+  defp do_react_stdio_path([]), do: raise("#{ReactPhoenix}: An installation of react-stdio cannot be found. Please run `npm install` and/or set the :react_stdio_path config value for :react_pheonix in your application.")
+  defp do_react_stdio_path([location | rest]) do
+    location = Path.expand(location)
     case File.exists?(location) do
       true -> location
       false -> do_react_stdio_path(rest)
