9838fd24ee164212bc3d4435fbc47d732e041c61
Tim Morton <tim@timothymorton.com>
2017-06-18 23:03:37 -0400

    Cleaner and more complete unit test for react_stdio_path

--- test/react_phoenix/react_io/path_finder_test.exs
+++ test/react_phoenix/react_io/path_finder_test.exs
@@ -1,6 +1,7 @@
 defmodule ReactPhoenix.ReactIo.PathFinderTest do
   use ExUnit.Case
   import ReactPhoenix.ReactIo.PathFinder
+  import ExUnit.CaptureLog
 
   test "react_stdio_path returns the first existing file" do
     assert react_stdio_path(["test", "does_not_exist"]) == "test"
@@ -8,6 +9,9 @@ defmodule ReactPhoenix.ReactIo.PathFinderTest do
   end
 
   test "react_stdio_path returns nil if no location is found" do
-    assert react_stdio_path([]) == nil
+    log_msg = capture_log(fn() ->
+      assert react_stdio_path([]) == nil
+    end)
+    assert log_msg =~ "react-stdio cannot be found"
   end
 end
