d82e10371e63a699b496943e15aace884f4e8070
Tim Morton <tim@sixwide.com>
2017-06-22 12:18:17 -0400

    Allow the package to compile without react-stdio presence at compile time (#19)
    * When the configuration sets react_stdio_path, don't search for it on compilation
    
    * When react-stdio cannot be found, print a warning instead of raising an error
    
    * Fix tests for PathFinder
    
    * Switched to Logger instead of IO.warn because IO.warn isn't available in Elixir 1.2
    
    * Cleaner and more complete unit test for react_stdio_path
    
    * Updated formatting and details of react-stdio warning message
    
    * update code style

--- lib/react_phoenix/react_io.ex
+++ lib/react_phoenix/react_io.ex
@@ -1,5 +1,7 @@
 defmodule ReactPhoenix.ReactIo do
   @moduledoc false
 
-  use StdJsonIo, otp_app: :react_phoenix, script: Application.get_env(:react_phoenix, :react_stdio_path, ReactPhoenix.ReactIo.PathFinder.react_stdio_path())
+  use StdJsonIo,
+    otp_app: :react_phoenix,
+    script: (Application.get_env(:react_phoenix, :react_stdio_path) || ReactPhoenix.ReactIo.PathFinder.react_stdio_path())
 end
--- test/react_phoenix/react_io/path_finder_test.exs
+++ test/react_phoenix/react_io/path_finder_test.exs
@@ -1,13 +1,17 @@
 defmodule ReactPhoenix.ReactIo.PathFinderTest do
   use ExUnit.Case
   import ReactPhoenix.ReactIo.PathFinder
+  import ExUnit.CaptureLog
 
   test "react_stdio_path returns the first existing file" do
     assert react_stdio_path(["test", "does_not_exist"]) == "test"
     assert react_stdio_path(["does_not_exist", "test"]) == "test"
   end
 
-  test "react_stdio_path raises an error if no location is found" do
-    assert_raise RuntimeError, fn -> react_stdio_path([]) end
+  test "react_stdio_path returns nil if no location is found" do
+    log_msg = capture_log(fn() ->
+      assert react_stdio_path([]) == nil
+    end)
+    assert log_msg =~ "react-stdio cannot be found"
   end
 end
